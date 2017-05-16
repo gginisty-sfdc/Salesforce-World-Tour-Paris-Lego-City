6650685d1103615a5e675395c69817c6501b0c6b
shocks13 <shocks13@gmail.com>
2017-05-16 12:52:28 +0100

    server edit

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/webhook', (req, res) => {
 });
 
 app.post('/webhook', (req, res) => {
-    console.log('req.body: ', req.body);
+    console.log('req: ', JSON.stringify(req));
     let events = req.body.entry[0].messaging;
     for (let i = 0; i < events.length; i++) {
         let event = events[i];
